f918629d6fc1fb4c035839595fef83d22ff41796
Ugo Sans H <glinel.ugo@gmail.com>
2019-06-12 13:37:00 +0200

    Merge confilict wzuo-master

--- src/material-datepicker.js
+++ src/material-datepicker.js
@@ -22,11 +22,31 @@
     MaterialDatepicker.prototype.init = function()
     {
         if (this.element_) {
-            this.datepicker_ = new epepite.DatePicker.DatePicker({
-                'input'  : this.element_,
-                'locale' : navigator.language ? navigator.language : 'fr'
-            });
+            var elementLang = this.element_.attributes['data-language'];
+            elementLang = elementLang && elementLang.value;
 
+            var pattern = this.element_.attributes['data-pattern'];
+            pattern = pattern && pattern.value;
+
+            var minDate = this.element_.attributes['data-min'];
+            minDate = minDate && moment(minDate.value, pattern).toDate();
+
+            var maxDate = this.element_.attributes['data-max'];
+            maxDate = maxDate && moment(maxDate.value, pattern).toDate();
+
+            var lang = elementLang ? elementLang : (navigator.language ? navigator.language : 'en');
+            var options = {
+                'input'   : this.element_,
+                'locale'  : lang,
+                'min_date': minDate,
+                'max_date': maxDate,
+            };
+
+            if (pattern) {
+              options['format'] = pattern;
+            }
+
+            this.datepicker_ = new epepite.DatePicker.DatePicker(options);
             this.datepicker_.on(epepite.DatePicker.CONSTANTS.DATEPICKER_EVENT, this.onSelectedDate.bind(this));
         }
     };
